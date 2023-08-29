9c030711bd9d1b059b8e390885c4c9fd378d747f
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-08-29 02:06:16 -0300

    feat (req10): Criado classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,31 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
+
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this._player1 = player1;
+    this._player2 = player2;
+  }
+
+  public get player1(): Fighter {
+    return this._player1;
+  }
+
+  public get player2(): Fighter {
+    return this._player2;
+  }
+
+  fight(): number {
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+    return super.fight();
+  }
+}
+
+export default PVP;
